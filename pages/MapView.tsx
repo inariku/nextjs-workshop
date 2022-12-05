901a76885c72d11927289fec110f957bc3f121d5
inariku <inariku@amazon.co.jp>
2022-12-05 12:23:31 +0900

    Initial Commit

---
+++ pages/MapView.tsx
@@ -0,0 +1,7 @@
+import { Geo } from 'aws-amplify'
+import { createMap } from 'maplibre-gl-js-amplify'
+import 'maplibre-gl-js-amplify/dist/public/amplify-geocoder.css'
+
+export default function MapView() {
+  return <div> Geo </div>
+}
